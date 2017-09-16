b57553eefcbb2f47301c911d13bd6ca1f8ae2020
aokabin <info.aokabin@gmail.com>
2017-09-16 15:48:39 +0900

    Create connection to local redis

---
+++ kvs/kvs.go
@@ -0,0 +1,20 @@
+package kvs
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+func KVSConnection() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
+	return client
+}
