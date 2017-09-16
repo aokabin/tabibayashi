c7bd9ab54a69ab8f4a8a52bcd7de94399288a70e
aokabin <info.aokabin@gmail.com>
2017-09-16 21:42:48 +0900

    Create kvs module for connect to redis server

--- kvs/kvs.go
+++ kvs/kvs.go
@@ -1,20 +1,58 @@
 package kvs
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/gob"
 
+	"github.com/aokabin/tabibayashi/config"
 	"github.com/go-redis/redis"
 )
 
-func KVSConnection() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+var (
+	c *redis.Client
+)
+
+type VisitData struct {
+	BeaconID string
+	SendDate string
+	Steps    string
+}
+
+func Connection() error {
+	host := config.RedisHost()
+	port := config.RedisPort()
+
+	c = redis.NewClient(&redis.Options{
+		Addr:     host + ":" + port,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
+	_, err := c.Ping().Result()
+	return err
+}
 
-	return client
+func addList(key string, value []byte) (int64, error) {
+	Connection() //TODO: 絶対もっといい方法ある
+	return c.RPush(key, value).Result()
+}
+
+func getLastListData(key string) (*VisitData, error) {
+	Connection() //TODO: 絶対もっといい方法ある
+	binVD, err := c.LIndex(key, -1).Result()
+	buf := bytes.NewBuffer([]byte(binVD))
+	var vd VisitData
+	gob.NewDecoder(buf).Decode(&vd)
+	return &vd, err
+}
+
+func AddVisitData(key string, vd *VisitData) (int64, error) {
+	buf := bytes.NewBuffer(nil)
+	gob.NewEncoder(buf).Encode(&vd)
+	return addList(key, buf.Bytes())
+}
+
+func GetLastVisitData(key string) (*VisitData, error) {
+	vd, err := getLastListData(key)
+	return vd, err
 }
---
+++ kvs/kvs_test.go
@@ -0,0 +1,46 @@
+package kvs
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddVisitData(t *testing.T) {
+	key := "user_sample"
+	vd := VisitData{
+		BeaconID: "10",
+		SendDate: "100000",
+		Steps:    "4000",
+	}
+
+	val, err := AddVisitData(key, &vd)
+
+	fmt.Println(val)
+
+	assert.Nil(t, err)
+	// assert.
+}
+
+func TestGetLastVisitData(t *testing.T) {
+	key := "x"
+	vd := VisitData{
+		BeaconID: "10",
+		SendDate: "100000",
+		Steps:    "5000",
+	}
+
+	val, err := AddVisitData(key, &vd)
+	fmt.Println(val)
+	assert.Nil(t, err)
+
+	lastVD, err := GetLastVisitData(key)
+
+	fmt.Println(vd)
+	fmt.Println(lastVD)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, vd)
+	assert.Equal(t, vd, *lastVD)
+}
